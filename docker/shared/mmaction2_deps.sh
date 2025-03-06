d2507342fe3bd3c9b6f3d62da89c8bc92526d86a
alvarogm <alvaro.gonzalezm@um.es>
2025-03-06 13:39:42 +0100

    install mmaction2 by source, add model
    - Changed mmaction2 package installation from mim to source
    - Add swin model config

--- docker/shared/mmaction2_deps.sh
+++ docker/shared/mmaction2_deps.sh
@@ -6,4 +6,11 @@ mim install mmengine
 mim install mmcv
 mim install mmdet
 mim install mmpose
-pip install mmaction2
+
+# Install mmaction2 by source (necessary for some utils it provides)
+git clone https://github.com/open-mmlab/mmaction2.git
+cd mmaction2
+pip install -v -e .
+# "-v" means verbose, or more output
+# "-e" means installing a project in editable mode,
+# thus any local modifications made to the code will take effect without re-installation.
